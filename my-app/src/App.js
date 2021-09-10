495b595a867dba85e660a1d3a358a8ed589ae36b
Yan <69123320+Yaneronin@users.noreply.github.com>
2021-09-10 23:26:32 +0300

    fix: completed hw2
    что-то не так с картинками, но выполнил согласно требованиям

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header.jsx";
 import { Layout } from "./components/Layout/Layout.jsx";
+import logo1 from "./components/Layout/bg2.jpg";
+import logo2 from "./components/Layout/bg1.jpg";
 
 function App() {
   return (
     <>
+      <Header title="Title" desc="description" />
+      <Layout title="Title" desc="description" urlBg={logo2} />
+      <Layout title="Title" desc="description" colorBg="#e2e2e2" />
+      <Layout title="Title" desc="description" urlBg={logo1} />
       <Footer />
-      <Layout />
-      <Layout />
-      <Layout />
-      <Header />
     </>
   );
 }
--- my-app/src/components/Layout/Layout.jsx
+++ my-app/src/components/Layout/Layout.jsx
@@ -1,14 +1,21 @@
-// import s from "./style.module.css";
+import s from "./style.module.css";
 const Layout = (props) => {
+  // const classes = s.desc + " " + s.full;
+  // const style = {
+  //   background: `url(${props.urlBg})`,
+  // };
+  const style = props.urlBg
+    ? { backgroundImage: `url(${props.urlBg})` }
+    : { background: `${props.colorBg}` };
   return (
-    <section className="s.root">
-      <div className="s.wrapper">
+    <section className={s.root} style={style}>
+      <div className={s.wrapper}>
         <article>
-          <div className="s.title">
+          <div className={s.title}>
             <h3> {props.title} </h3>
-            <span className="s.separator"></span>
+            <span className={s.separator}></span>
           </div>
-          <div className="s.desc full">
+          <div>
             <p>{props.desc}</p>
           </div>
         </article>
